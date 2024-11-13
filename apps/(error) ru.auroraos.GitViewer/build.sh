2b1df4d0bc93e88e1f4b87c4f60a4d994e767851
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-11-13 19:49:35 +0300

    [feature] ru.auroraos.GitViewer & ru.auroraos.RSSReader

---
+++ apps/(error) ru.auroraos.GitViewer/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Get psdk
+chroot=$(aurora-cli api --route='/psdk/installed' | grep 'aurora_psdk/sdk-chroot' | head -n 1 | xargs | sed 's/,//g')
+version=$($chroot version | tail -1 | awk -F' ' '{print $2}')
+
+target_arm=$(aurora-cli api --route="/psdk/targets?version=$version" | grep -E '"AuroraOS.+armv7hl"' | head -n 1 | xargs | sed 's/,//g')
+target_arm64=$(aurora-cli api --route="/psdk/targets?version=$version" | grep -E '"AuroraOS.+aarch64"' | head -n 1 | xargs | sed 's/,//g')
+target_x64=$(aurora-cli api --route="/psdk/targets?version=$version" | grep -E '"AuroraOS.+x86_64"' | head -n 1 | xargs | sed 's/,//g')
+
+# Set projects dir
+DIR="$(dirname "$(realpath "$0")")"
+cd $DIR
+mkdir -p ../../projects
+cd ../../projects
+
+PROJECT='gitviewer'
+
+# Clone project
+if [ -d "$PROJECT" ]; then
+    cd $PROJECT
+    git clean -fdx
+    git pull
+else
+    git clone https://gitlab.com/omprussia/kmp/gitviewer.git
+    cd $PROJECT
+fi
+
+# Build kmp
+chmod +x ./gradlew
+./gradlew jsBuildForAurora
+
+# Open folder Aurora OS
+cd auroraApp/GitViewer
+
+# Folder builds
+rm -rf $DIR/builds
+mkdir $DIR/builds
+
+# Build arm
+$chroot mb2 --no-fix-version --target $target_arm build
+mv ./RPMS/* $DIR/builds && git clean -fdx
+
+# Build arm64
+$chroot mb2 --no-fix-version --target $target_arm64 build
+mv ./RPMS/* $DIR/builds && git clean -fdx
+
+# Build x64
+$chroot mb2 --no-fix-version --target $target_x64 build
+mv ./RPMS/* $DIR/builds && git clean -fdx
